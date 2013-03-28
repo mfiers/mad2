a4a59f46d250d9076a794ead0cd6bdafb47c14a4
Mark Fiers <mark.fiers.42@gmail.com>
2013-03-28 17:04:54 +0100

    add onthefly data to the madfile object & store data in mongodb

---
+++ mad2/plugin/mongo.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+
+import logging
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import leip
+import copy
+import datetime
+import pprint
+from mad2.util import get_all_mad_files
+
+
+lg = logging.getLogger(__name__)
+lg.setLevel(logging.DEBUG)
+
+@leip.hook("madfile_save")
+def store_in_mongodb(app, madfile):
+    lg.debug("running store_in_mongodb")
+    mng_mad, mng_hist = _get_mng_collection(app)
+    if 'mongoid' in madfile.mad:
+        mongoid = madfile.mad.mongoid
+        hist_rec = mng_mad.find_one({'_id': ObjectId(mongoid)})
+        #keep historical record
+        del(hist_rec["_id"])
+        hist_rec['mongoid'] = mongoid
+        hist_rec['date'] = datetime.datetime.now()
+        mng_hist.insert(hist_rec)
+        #store record
+        newrec = madfile.data()
+        del(newrec['mongoid'])
+        newrec['_id'] = ObjectId(mongoid)
+        mng_mad.save(newrec)
+    else:
+        mongoid = mng_mad.insert(madfile.data())
+        lg.debug("Updating mongo record {0}".format(mongoid))
+        madfile.mad.mongoid = str(mongoid)
+
+def _get_mng_collection(app):
+    host = app.conf.plugin.mongo.host
+    port = app.conf.plugin.mongo.get('port', 27017)
+    client = MongoClient(host, port)
+    return client.mad.mad, client.mad.history
+
+@leip.arg('file', nargs="+")
+@leip.command
+def mongo_show(app, args):
+    """
+    Show the associated mongodb record  
+    """
+    mng_mad, mng_hist = _get_mng_collection(app)
+    for madfile in get_all_mad_files(app, args):
+        if 'mongoid' in madfile.mad:
+            mongoid = madfile.mad.mongoid
+            rec = mng_mad.find_one({'_id': ObjectId(mongoid)})
+            print(madfile.filename)
+            for key in rec:
+                print('- {0}: {1}'.format(key, rec[key]))
+        
---
+++ mad2/plugin/onthefly.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+import os
+import logging
+import socket
+import leip
+from pwd import getpwuid
+import hashlib
+
+lg = logging.getLogger(__name__)
+#lg.setLevel(logging.DEBUG)
+
+def qd_hash(filename):
+    """
+    Provde a quick & dirty hash
+
+    this is by no means secure, but quick for very large files, and as long
+    as one does not try to create duplicate hashes, the chance is still very
+    slim that a duplicate will arise
+    """
+    sha1sum = hashlib.sha1()
+    filesize = os.stat(filename).st_size 
+    if filesize < 10000:
+        with open(filename) as F:
+            sha1sum.update(F.read())
+    else:
+        with open(filename) as F:
+            sha1sum.update(F.read(2000))
+            F.seek(int(filesize * 0.4))
+            sha1sum.update(F.read(2000))
+            F.seek(-2000, 2)
+            sha1sum.update(F.read())
+    return sha1sum.hexdigest()
+
+@leip.hook("madfile_load")
+def onthefly(app, madfile):
+    lg.debug("running onthelfy")
+    madfile.otf.fullpath = os.path.abspath(madfile.filename)
+    madfile.otf.host = socket.getfqdn()
+    filestat = os.stat(madfile.filename)
+    madfile.otf.filesize = filestat.st_size
+    userinfo = getpwuid(filestat.st_uid)
+    madfile.otf.userid = userinfo.pw_name 
+    madfile.otf.username = userinfo.pw_gecos
+    madfile.otf.qdhash = qd_hash(madfile.filename)
