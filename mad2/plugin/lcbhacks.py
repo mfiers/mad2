6fdd132bfa5f72e9006437a12c11ffa2affcfd9a
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-17 20:42:11 +0200

    LCB hacks work now

--- mad2/plugin/lcbhacks.py
+++ mad2/plugin/lcbhacks.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 
 import logging
 import leip
+import os
+import re
 
 from mad2.util import get_all_mad_files
 
@@ -11,9 +13,17 @@ lg = logging.getLogger(__name__)
 def lbconthefly(app, madfile):
 
     fullpath = madfile.otf.fullpath
-    if '/media/seq-srv-04' in fullpath:
-        madfile.otf.host = 'SEQ_SRV_04'
-    if '/media/seq-srv-05' in fullpath:
-        madfile.otf.host = 'SEQ_SRV_05'
-    if '/media/seq-srv-06' in fullpath:
-        madfile.otf.host = 'SEQ_SRV_06'
+    mtcher = re.compile(r'/media/(seq-srv-[0-9][0-9])', re.I)
+    mtch = mtcher.search(fullpath)
+    if not mtch:
+        return
+
+
+    server = mtch.groups()[0]
+
+    madfile.otf.host = server.upper()
+    fp = madfile.otf.fullpath
+    fp = fp[fp.index(server) + len(server):]
+    dn = os.path.dirname(fp)
+    madfile.otf.fullpath = fp
+    madfile.otf.dirname = dn
