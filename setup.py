56cf6743bc191663dda0211c4033857e8a3c7592
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-17 20:42:26 +0200

    to version 0.0.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ entry_points = {
         ]}
 
 setup(name='mad2',
-      version='0.0.11',
+      version='0.0.12',
       description=DESCRIPTION,
       author='Mark Fiers',
       author_email='mark.fiers42@gmail.com',
