81da6872140a7bc7c560ed801316d5e29baa3600
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-17 22:15:39 +0200

    always track a quick & dirty hash to check if a file MIGHT have changed

--- mad2/plugin/checksum.py
+++ mad2/plugin/checksum.py
@@ -11,28 +11,6 @@ from mad2.util import get_all_mad_files
 lg = logging.getLogger(__name__)
 
 
-def _get_qdhash(filename):
-    """
-    Provde a quick & dirty hash
-
-    this is by no means secure, but quick for very large files, and as long
-    as one does not try to create duplicate hashes, the chance is still very
-    slim that a duplicate will arise
-    """
-    sha1sum = hashlib.sha1()
-    filesize = os.stat(filename).st_size
-    if filesize < 10000:
-        with open(filename) as F:
-            sha1sum.update(F.read().encode())
-    else:
-        with open(filename) as F:
-            sha1sum.update(F.read(2000))
-            F.seek(int(filesize * 0.4))
-            sha1sum.update(F.read(2000))
-            F.seek(-2000, 2)
-            sha1sum.update(F.read())
-    return sha1sum.hexdigest()
-
 def hashit(hasher, filename):
     """
     Provde a quick & dirty hash
@@ -82,20 +60,4 @@ def sha1(app, args):
         madfile.save()
         print(madfile.filename)
 
-@leip.arg('-f', '--force', action='store_true', help='apply force')
-@leip.arg('file', nargs='*')
-@leip.command
-def qdhash(app, args):
-    """
-    Calculate a sha1 checksum
-    """
-    for madfile in get_all_mad_files(app, args):
-        if not args.force and 'qdhash' in madfile.mad:
-            #exists - and not forcing
-            lg.warning("Skipping sha1 checksum - exists")
-            continue
-        cs = _get_qdhash(madfile.filename)
-        madfile.mad.hash.qdhash = cs
-        madfile.save()
-        print(madfile.filename)
 
