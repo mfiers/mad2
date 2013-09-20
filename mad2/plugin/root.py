23fe29aa35664a363c655ef2074da11291859253
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-20 16:15:53 +0200

    Simple plugin that sets file permissions & ownership to that of the original file when mad is executed as root

---
+++ mad2/plugin/root.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import logging
+import leip
+import os
+import re
+
+from mad2.util import get_all_mad_files
+
+lg = logging.getLogger(__name__)
+
+
+@leip.hook("madfile_save", 100)
+def lbconthefly(app, madfile):
+
+    if os.geteuid() != 0:
+        #only do something when root
+        return
+
+    lg.critical("changing .madfile permission")
+
+    filename = madfile.otf.filename
+    madname = madfile.otf.madname
+
+    fstats = os.stat(filename)
+    os.chmod(madname, fstats.st_mode)
+    os.chown(madname, fstats.st_uid, fstats.st_gid)
