74f504ca2b448a1532d248b40837ccc589e2fc6e
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-23 15:27:24 +0200

    bump to 0.0.22

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ entry_points = {
         ]}
 
 setup(name='mad2',
-      version='0.0.21',
+      version='0.0.22',
       description=DESCRIPTION,
       author='Mark Fiers',
       author_email='mark.fiers42@gmail.com',
