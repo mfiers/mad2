1139b1360ce43585ec9eb352462a266b0151270c
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-23 14:24:22 +0200

    read sha1/md5 in chunks

--- mad2/plugin/checksum.py
+++ mad2/plugin/checksum.py
@@ -2,13 +2,50 @@ from __future__ import print_function
 
 import logging
 import sys
-
 import os
 import leip
 import hashlib
+
 from mad2.util import get_all_mad_files
 
 lg = logging.getLogger(__name__)
+
+def get_qdhash(filename):
+    """
+    Provde a quick & dirty hash - a good indication that a file MIGHT have
+    changed - but by no means secure.
+
+    It is quick, though.
+    """
+    sha1sum = hashlib.sha1()
+    filesize = os.stat(filename).st_size
+    if filesize < 5000:
+        with open(filename) as F:
+            sha1sum.update(F.read().encode())
+    else:
+        with open(filename) as F:
+            sha1sum.update(F.read(2000))
+            F.seek(int(filesize * 0.4))
+            sha1sum.update(F.read(2000))
+            F.seek(-2000, 2)
+            sha1sum.update(F.read())
+    return sha1sum.hexdigest()
+
+
+@leip.hook("madfile_load", 150)
+def qdhash(app, madfile):
+    """
+    Calculate a sha1 checksum
+    """
+    cs = get_qdhash(madfile.filename)
+
+    if madfile.mad.hash.qdhash:
+        qdh = madfile.mad.hash.qdhash
+        if qdh != cs:
+            print("{} has changed!".format(madfile.filename),
+                file=sys.stderr)
+
+    madfile.mad.hash.qdhash = cs
 
 
 def hashit(hasher, filename):
@@ -20,8 +57,10 @@ def hashit(hasher, filename):
     slim that a duplicate will arise
     """
     h = hasher()
-    with open(filename) as F:
-        h.update(F.read())
+    blocksize = 2**20
+    with open(filename, 'rb') as F:
+        for chunk in iter(lambda: F.read(blocksize), b''):
+            h.update(chunk)
     return h.hexdigest()
 
 
