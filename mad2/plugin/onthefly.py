27e2d458ddcbc815e9b70053bb0b745b29327a54
Mark Fiers <mark.fiers.42@gmail.com>
2013-09-27 15:35:53 +0200

    adding mtime, atime to on the fly data

--- mad2/plugin/onthefly.py
+++ mad2/plugin/onthefly.py
@@ -1,37 +1,39 @@
 from __future__ import print_function
 
+from datetime import datetime
 import os
 import logging
 import socket
-import sys
 
 import leip
 from pwd import getpwuid
 
-from mad2.util import get_all_mad_files
-
 lg = logging.getLogger(__name__)
 
-FIEX = None
+EXTENSION_DATA = None
+
 
 def get_fiex(app):
-    global FIEX
-    if not FIEX is None:
-        return FIEX
+    global EXTENSION_DATA
+    if not EXTENSION_DATA is None:
+        return EXTENSION_DATA
 
-    FIEX = {}
+    EXTENSION_DATA = {}
     for ft in app.conf.filetype:
         for ext in app.conf.filetype.get(ft).extensions:
-            FIEX[ext] = ft
+            EXTENSION_DATA[ext] = ft
+
+    return EXTENSION_DATA
+
 
 def apply_file_format(app, madfile, filename=None):
 
-    fiex = get_fiex(app)
+    extension_data = get_fiex(app)
 
     if filename is None:
         filename = madfile.otf.basename
 
-    splitter = filename.rsplit('.',1)
+    splitter = filename.rsplit('.', 1)
     if len(splitter) != 2:
         return
 
@@ -40,17 +42,16 @@ def apply_file_format(app, madfile, filename=None):
     #this ensures that the innermost extension seen is stored
     madfile.otf.extension = ext
 
-    if not ext in FIEX:
+    if not ext in extension_data:
         return
 
-    ft = FIEX[ext]
+    ft = extension_data[ext]
     ftinfo = app.conf.filetype.get(ft)
 
     lg.debug("identified filetype %s" % ft)
     if ftinfo.template:
         template = app.conf.template.get(ftinfo.template)
         madfile.otf.update(template)
-
 
     if ftinfo.get('continue', False):
         lg.debug("contiue filetype disocvery on: %s" % base)
@@ -59,15 +60,33 @@ def apply_file_format(app, madfile, filename=None):
 
 @leip.hook("madfile_load")
 def onthefly(app, madfile):
+
+    if sorted(list(madfile.mad.keys())) == ['hash']:
+        madfile.otf.annotated = False
+    else:
+        madfile.otf.annotated = True
+
     lg.debug("running onthelfy")
     madfile.otf.fullpath = os.path.abspath(madfile.filename)
     lg.debug("get fqdn")
     madfile.otf.host = socket.gethostname()
+
     filestat = os.stat(madfile.filename)
     madfile.otf.filesize = filestat.st_size
     userinfo = getpwuid(filestat.st_uid)
     madfile.otf.userid = userinfo.pw_name
     madfile.otf.username = userinfo.pw_gecos
+
+    mtime = datetime.utcfromtimestamp(
+        filestat.st_mtime)
+    atime = datetime.utcfromtimestamp(
+        filestat.st_atime)
+
+    madfile.otf.atime = atime.isoformat()
+    madfile.otf.atime_simple = atime.strftime("%m%y")
+
+    madfile.otf.mtime = mtime.isoformat()
+    madfile.otf.mtime_simple = mtime.strftime("%m%y")
 
     apply_file_format(app, madfile)
 
