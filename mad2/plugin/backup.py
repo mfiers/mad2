60edf9fcfa26287a8cbce3d7699f3ba3eedbd435
Mark Fiers <Mark.Fiers@cme.vib-kuleuven.be>
2014-01-17 14:40:57 +0100

    shortcut to mark files for backup

---
+++ mad2/plugin/backup.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import logging
+
+import leip
+from mad2.util import get_all_mad_files
+
+lg = logging.getLogger(__name__)
+
+
+@leip.arg('file', nargs='*')
+@leip.command
+def backup(app, args):
+    """
+    Shortcut to mark a file for backup
+    """
+    for madfile in get_all_mad_files(app, args):
+    	madfile.mad.backup = True
+        madfile.save()
+
