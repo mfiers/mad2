e3118427eee8f86d4a04bc57afcaa29b4612d151
Mark Fiers <mark.fiers.42@gmail.com>
2014-02-07 13:02:03 +0100

    moved mad set/unset to plugin

---
+++ mad2/plugin/madset.py
@@ -0,0 +1,199 @@
+
+import logging
+import sys
+
+from dateutil.parser import parse as dateparse
+import leip
+
+from mad2.util import  get_mad_file, get_all_mad_files
+
+lg = logging.getLogger(__name__)
+
+
+##
+## define unset
+##
+@leip.arg('file', nargs="+")
+@leip.arg('key')
+@leip.arg('-e', '--echo', help='echo ')
+@leip.command
+def unset(app, args):
+    lg.debug("unsetting: %s".format(args.key))
+    key = args.key
+    keywords = app.conf.keywords
+    keyinfo = keywords[key]
+    if keyinfo.cardinality =='+':
+        print("Not implemented - unsetting keys with cardinality > 1")
+        sys.exit(-1)
+
+    for madfile in get_all_mad_files(app, args):
+        #print(madfile)
+        #print(madfile.mad.pretty())
+        if args.key in madfile.mad:
+            del(madfile.mad[args.key])
+
+        madfile.save()
+
+
+@leip.arg('-f', '--force', action='store_true', help='apply force')
+@leip.arg('-p', '--prompt', action='store_true', help='show a prompt')
+@leip.arg('-e', '--editor', action='store_true', help='open an editor')
+@leip.arg('file', nargs='*')
+@leip.arg('value', help='value to set', nargs='?')
+@leip.arg('key', help='key to set')
+@leip.command
+def set(app, args):
+    """
+    Set a key/value for one or more files.
+
+    Use this command to set a key value pair for one or more files.
+
+    This command can take the following forms::
+
+        mad set project test genome.fasta
+        ls *.fasta | mad set project test
+        find . -size +10k | mad set project test
+
+    """
+
+    key = args.key
+    val = args.value
+
+    if args.prompt or args.editor:
+        if not args.value is None:
+            # when asking for a prompt - the next item on sys.argv
+            # is assumed to be a file, and needs to be pushed
+            # into args.file
+            args.file = [args.value] + args.file
+
+    madfiles = []
+
+    #gather all madfiles for later parsing
+    for m in get_all_mad_files(app, args):
+        madfiles.append(m)
+
+    #check if mad needs to show a prompt or editor
+    if val is None and not (args.prompt or args.editor):
+        args.prompt = True
+
+
+    # show prompt or editor
+    if args.prompt or args.editor:
+        # get a value from the user
+
+        default = ''
+        #Show a prompt asking for a value
+        if len(madfiles) == 1:
+            data = madfiles[0].data(app.conf)
+            default = madfiles[0].mad.get(key, "")
+        else:
+            data = app.conf.simple()
+
+        if args.prompt:
+            sys.stdin = open('/dev/tty')
+            val = mad2.ui.askUser(key, default, data)
+            sys.stdin = sys.__stdin__
+
+        elif args.editor:
+            editor = os.environ.get('EDITOR','vim')
+            tmp_file = tempfile.NamedTemporaryFile('wb', delete=False)
+
+            #write default value to the tmp file
+            if default:
+                tmp_file.write(default + "\n")
+            else:
+                tmp_file.write("\n")
+            tmp_file.close()
+
+            tty = open('/dev/tty')
+
+            subprocess.call('{} {}'.format(editor, tmp_file.name),
+                stdin=tty, shell=True)
+            sys.stdin = sys.__stdin__
+
+
+            #read value back in
+            with open(tmp_file.name, 'r') as F:
+                #removing trailing space
+                val = F.read().rstrip()
+            #remove tmp file
+            os.unlink(tmp_file.name)
+
+
+    # Now process madfiles
+    lg.debug("processing %d files" % len(madfiles))
+
+    # First check if
+    list_mode = False
+    if key[0] == '+':
+        lg.debug("treating {} as a list".format(key))
+        list_mode = True
+        key = key[1:]
+
+    keywords = app.conf.keywords
+    keyinfo = keywords[key]
+    keytype = keyinfo.get('type', 'str')
+
+    if list_mode and keyinfo.cardinality == '1':
+        print("Cardinality == 1 - no lists!")
+        sys.exit(-1)
+    elif keyinfo.cardinality == '+':
+        list_mode = True
+
+
+    if not args.force and not key in keywords:
+        print("invalid key: {0} (use -f?)".format(key))
+        sys.exit(-1)
+
+
+    if keytype == 'int':
+        try:
+            val = int(val)
+        except ValueError:
+            lg.error("Invalid integer: %s" % val)
+            sys.exit(-1)
+    elif keytype == 'float':
+        try:
+            val = float(val)
+        except ValueError:
+            lg.error("Invalid float: %s" % val)
+            sys.exit(-1)
+    elif keytype == 'boolean':
+        if val.lower() in ['1', 'true', 't', 'yes', 'aye', 'y', 'yep']:
+            val = True
+        elif val.lower() in ['0', 'false', 'f', 'no', 'n', 'nope']:
+            val = False
+        else:
+            lg.error("Invalid boolean: %s" % val)
+            sys.exit(-1)
+    elif keytype == 'date':
+        try:
+            val = dateparse(val)
+        except ValueError:
+            lg.error("Invalid date: %s" % val)
+            sys.exit(-1)
+        lg.debug("date interpreted as: %s" % val)
+
+    if keytype == 'restricted' and \
+            not val in keyinfo.allowed:
+        print("Value '{0}' not allowed".format(val))
+        sys.exit(-1)
+
+    for madfile in madfiles:
+        lg.debug('madmadmad')
+        if list_mode:
+            if not key in madfile.mad:
+                oldval = []
+            else:
+                oldval = madfile.mad[key]
+                if not isinstance(oldval, list):
+                    oldval = [oldval]
+            madfile.mad[key] = oldval + [val]
+            madfile.all[key] = oldval + [val]
+
+        else:
+            #not listmode
+            madfile.mad[key] = val
+            madfile.all[key] = val
+
+        madfile.save()
