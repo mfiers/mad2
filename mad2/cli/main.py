a03ea5d58a3c93be0e34a9b6965d4bbe7f415b76
Mark Fiers <mark.fiers.42@gmail.com>
2014-09-19 10:00:34 +0200

    added a --trust parameter, which disables sha1sum checking

--- mad2/cli/main.py
+++ mad2/cli/main.py
@@ -37,6 +37,7 @@ def dispatch():
         import os
         import pstats
         import tempfile
+
         pr = cProfile.Profile()
         pr.enable()
         app.run()
@@ -178,6 +179,6 @@ def has_command(app, args):
 #
 
 app = leip.app('mad2')
-
+app.parser.add_argument('--trust', action='store_true', help='trust sha1sum')
 # discover hooks in this module!
 app.discover(globals())
