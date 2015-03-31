de77ff31d21d6ea41678ec3b06b17c4333a014be
Mark Fiers <mark.fiers.42@gmail.com>
2015-03-31 10:46:13 +0200

    define metadata from an ipython notebook

--- mad2/ipy.py
+++ mad2/ipy.py
@@ -2,31 +2,28 @@
 import re
 import sys
 
+from IPython.core.magic import (register_line_magic, register_cell_magic,
+                                register_line_cell_magic)
+
 from IPython.core.magic import magics_class, line_magic, cell_magic, Magics
 from IPython.display import HTML
 
 def error(*msg):
     sys.stderr.write(" ".join(map(str, msg)))
 
-@magics_class
-class MadMadMad(Magics):
-    @line_magic
-    def mad_set(self, line):
-        if not ' ' in line:
-            error("invalid metadata set", line)
-            return            
-        k,v = line.strip().split(None, 1)
-        if not re.match(r'^[A-Za-z][\w\.]+$', k):
-            error("Invalid key:", k)
-            return
-        js = ["<script>"]
-        js.append("var md = IPython.notebook.metadata;")
-        js.append("if (!('mad' in md)) md.mad = {};")
-        js.append("md.mad.{} = '{}';".format(k, v))
-        js.append("</script>")
-        return HTML(" ".join(js))
+@register_line_magic
+def mad_set(line):
+    if not ' ' in line:
+        error("invalid metadata set", line)
+        return            
+    k,v = line.strip().split(None, 1)
+    if not re.match(r'^[A-Za-z][\w\.]+$', k):
+        error("Invalid key:", k)
+        return
+    js = ["<script>"]
+    js.append("var md = IPython.notebook.metadata;")
+    js.append("if (!('mad' in md)) md.mad = {};")
+    js.append("md.mad.{} = '{}';".format(k, v))
+    js.append("</script>")
+    return HTML(" ".join(js))
 
-    
-def load_ipython_extension(ip):
-    ip.register_magics(MadMadMad)
-            
